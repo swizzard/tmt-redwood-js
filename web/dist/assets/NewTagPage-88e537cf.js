f711e6d8804c8134f08fc5eba0c9d59f09bffc46
Sam Raker <sam.raker@gmail.com>
2024-05-21 13:44:54 -0400

    no dist

--- web/dist/assets/NewTagPage-88e537cf.js
+++
@@ -1,7 +0,0 @@
-import{g,a as m,d as s,j as a}from"./index-65dccdee.js";import{t}from"./index-e3e85086.js";import{T as c}from"./TagForm-3f811310.js";import"./index-574b20c6.js";const d=g`
-  mutation CreateTagMutation($input: CreateTagInput!) {
-    createTag(input: $input) {
-      id
-    }
-  }
-`,u=()=>{const[r,{loading:o,error:n}]=m.useMutation(d,{onCompleted:()=>{t.toast.success("Tag created"),s.navigate(s.routes.tags())},onError:e=>{t.toast.error(e.message)}}),i=e=>{r({variables:{input:e}})};return a.jsxs("div",{className:"rw-segment",children:[a.jsx("header",{className:"rw-segment-header",children:a.jsx("h2",{className:"rw-heading rw-heading-secondary",children:"New Tag"})}),a.jsx("div",{className:"rw-segment-main",children:a.jsx(c,{onSave:i,loading:o,error:n})})]})},h=()=>a.jsx(u,{});export{h as default};
--- web/dist/assets/NewUserPage-b43a3d14.js
+++
@@ -1,7 +0,0 @@
-import{g as m,a as c,d as r,j as e}from"./index-65dccdee.js";import{t}from"./index-e3e85086.js";import{U as d}from"./UserForm-3392ac95.js";import"./index-574b20c6.js";const u=m`
-  mutation CreateUserMutation($input: CreateUserInput!) {
-    createUser(input: $input) {
-      id
-    }
-  }
-`,l=()=>{const[a,{loading:o,error:n}]=c.useMutation(u,{onCompleted:()=>{t.toast.success("User created"),r.navigate(r.routes.users())},onError:s=>{t.toast.error(s.message)}}),i=s=>{a({variables:{input:s}})};return e.jsxs("div",{className:"rw-segment",children:[e.jsx("header",{className:"rw-segment-header",children:e.jsx("h2",{className:"rw-heading rw-heading-secondary",children:"New User"})}),e.jsx("div",{className:"rw-segment-main",children:e.jsx(d,{onSave:i,loading:o,error:n})})]})},h=()=>e.jsx(l,{});export{h as default};
